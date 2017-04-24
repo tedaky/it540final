faa2ef9529a3f89b1f79aba7717839665810c47e
Eric Tiedeken <eric.tiedeken@gmail.com>
2017-04-24 17:50:43 -0500

    Return all movies that have had sold out showings

--- FinalProjectReturnQueries.sql
+++ FinalProjectReturnQueries.sql
@@ -11,3 +11,19 @@ WHERE NOT EXISTS (
 	JOIN [OrderDetail] [od]
 	ON [t].[TicketID] = [od].[TicketID]
 );
+GO
+
+-- Return all the movies that have had sold out showings.
+SELECT [m].[MovieID], [m].[Title]
+FROM [Movie] [m]
+WHERE EXISTS (
+	SELECT [a].[Available_Seats]
+	FROM [Auditorium] [a]
+	JOIN [Showing] [s]
+	ON [s].[AuditoriumID] = [a].[AuditoriumID]
+	JOIN [Ticket] [t]
+	ON [t].[ShowingID] = [s].[ShowingID]
+	JOIN [OrderDetail] [od]
+	ON [od].[TicketID] = [t].TicketID
+	WHERE [od].[No_of_Tickets] = [a].[Available_Seats]
+);
