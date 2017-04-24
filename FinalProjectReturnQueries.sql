0d036620d54eeaa01b4b941abcc45a0ce345d995
Eric Tiedeken <eric.tiedeken@gmail.com>
2017-04-24 18:08:30 -0500

    Return average age of moviegoers per movie. Return number of moviegoers per category

--- FinalProjectReturnQueries.sql
+++ FinalProjectReturnQueries.sql
@@ -27,3 +27,29 @@ WHERE EXISTS (
 	ON [od].[TicketID] = [t].TicketID
 	WHERE [od].[No_of_Tickets] = [a].[Available_Seats]
 );
+GO
+
+-- What’s the average age of the moviegoers per movie.
+SELECT AVG(DATEDIFF(YEAR, [c].[DOB], GETDATE())) AS 'Average_Age_Per_Movie', [m].[MovieID], [m].[Title]
+FROM [Customer] [c]
+JOIN [Order] [o]
+ON [o].[CustomerID] = [c].[CustomerID]
+JOIN [OrderDetail] [od]
+ON [od].[OrderID] = [o].[OrderID]
+JOIN [Ticket] [t]
+ON [t].[TicketID] = [od].[TicketID]
+JOIN [Showing] [s]
+ON [s].[ShowingID] = [t].[ShowingID]
+JOIN [Movie] [m]
+ON [m].[MovieID] = [s].[MovieID]
+GROUP BY [m].[MovieID], [m].[Title];
+GO
+
+-- Return per category the number of moviegoers.
+SELECT [c].[CategoryID], [c].[Name], SUM([od].[No_of_Tickets]) AS 'Sum_Of_Tickets'
+FROM [Category] [c]
+JOIN [Ticket] [t]
+ON [t].[CategoryID] = [c].[CategoryID]
+JOIN [OrderDetail] [od]
+ON [od].[TicketID] = [t].[TicketID]
+GROUP BY [c].[CategoryID], [c].[Name]
